0aad5d0219a6bee0bda4774dbd2516959072def5
vagrant <vagrant@precise32.(none)>
2014-02-20 19:06:22 +0000

    putting it together lab

--- routes/project.js
+++ routes/project.js
@@ -5,6 +5,9 @@ exports.projectInfo = function(req, res) { 
 
   // query for the specific project and
   // call the following callback
+  models.Project
+    .find({"_id": projectID})
+    .exec(afterQuery);
 
   function afterQuery(err, projects) {
     if(err) console.log(err);
@@ -18,6 +21,19 @@ exports.addProject = function(req, res) {
 
   // make a new Project and save it to the DB
   // YOU MUST send an OK response w/ res.send();
+  var newPost = new models.Project({
+    "title": form_data["project_title"],
+    "date": form_data["date"],
+    "summary": form_data["summary"],
+    "image": form_data["image_url"],
+  });
+
+  newPost.save(afterSaving);
+
+  function afterSaving(err) {
+    if(err) {console.log(err); res.send(500); }
+    res.redirect('/'); 
+  }
 }
 
 exports.deleteProject = function(req, res) {
@@ -25,4 +41,13 @@ exports.deleteProject = function(req, res) {
 
   // find the project and remove it
   // YOU MUST send an OK response w/ res.send();
+  models.Project
+    .find({"_id": projectID})
+    .remove()
+    .exec(afterRemove)
+
+  function afterRemove() {
+    res.send(200);
+  }
+
 }
